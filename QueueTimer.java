a0acc4211e4bda5df0d59b69de375a834903adb7
Jelle de Boer <jrj.deboer@me.com>
2015-02-06 11:47:54 +0100

    queuetimer

--- QueueTimer.java
+++ QueueTimer.java
@@ -1,3 +1,24 @@
 public class QueueTimer{
-	
+
+  // Adds an Integer object to the queue.
+  public void addElement(java.lang.Integer elem){
+
+  }
+
+  // Removes an object from the queue.
+  public void removeElement(){
+
+  }
+
+  // Returns the size of the queue.
+  public int getSize(){
+
+  }
+
+  // Tells whether the queue is empty.
+  public boolean isEmpty(){
+
+  }
+
+
 }
