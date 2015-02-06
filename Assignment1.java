00280c63890895c8535a99af93da1fd4ef271b1b
Petra <p_pinoa@hotmail.com>
2015-02-06 11:36:10 +0100

    itgitv
    y80h

---
+++ Assignment1.java
@@ -0,0 +1,33 @@
+import java.lang.Object.*;
+
+public class Assignment1{
+
+	private  java.util.ArrayList<java.util.List<java.lang.Integer>> lists
+
+	private  java.util.ArrayList<java.util.Queue<java.lang.Integer>> queues
+
+
+	public Assignment1(){
+
+	}
+
+	public void benchmark(){
+
+	}
+
+	public void benchmark(long elemGenSeed){
+
+	}
+
+	public void benchmark(long elemGenSeed, int[] mutations){
+
+	}
+
+	public static void main(java.lang.String[] args){
+
+	}
+
+	private static void errorExit(java.lang.String msg){
+
+	}
+}
